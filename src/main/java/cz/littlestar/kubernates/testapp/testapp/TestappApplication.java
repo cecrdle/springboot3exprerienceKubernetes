03a4698f9de26068b4d91e75cdd3b90518f424f9
cecrdlem <cecrdle.milan@hypotecnibanka.cz>
2023-10-13 23:50:04 +0200

    Initial commit

---
+++ src/main/java/cz/littlestar/kubernates/testapp/testapp/TestappApplication.java
@@ -0,0 +1,20 @@
+package cz.littlestar.kubernates.testapp.testapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TestappApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello world";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(TestappApplication.class, args);
+	}
+
+}
