2b94ac228671571f49df4e2ac9de23d381bb8260
Milan Čečrdle <milan.cecrdle@gmail.com>
2025-06-25 15:07:30 +0200

    Add MockMvc test for root endpoint

--- src/test/java/cz/littlestar/kubernates/testapp/testapp/TestappApplicationTests.java
+++ src/test/java/cz/littlestar/kubernates/testapp/testapp/TestappApplicationTests.java
@@ -1,13 +1,30 @@
 package cz.littlestar.kubernates.testapp.testapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class TestappApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+       @Autowired
+       private MockMvc mockMvc;
+
+       @Test
+       void contextLoads() {
+       }
+
+       @Test
+       void homeEndpointReturnsHelloWorld() throws Exception {
+               mockMvc.perform(get("/"))
+                               .andExpect(status().isOk())
+                               .andExpect(content().string("Hello world"));
+       }
 
 }
