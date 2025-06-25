a86e93f88234b0147f64719b1d36723badcd5373
Milan Čečrdle <milan.cecrdle@gmail.com>
2025-06-25 15:04:37 +0200

    Rename package to kubernetes

--- src/test/java/cz/littlestar/kubernetes/testapp/testapp/TestappApplicationTests.java
+++ src/test/java/cz/littlestar/kubernetes/testapp/testapp/TestappApplicationTests.java
@@ -1,4 +1,4 @@
-package cz.littlestar.kubernates.testapp.testapp;
+package cz.littlestar.kubernetes.testapp.testapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
